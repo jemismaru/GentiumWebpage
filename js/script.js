3a2843a36d7e3ce87afab091d7eaa82bfc31cde5
Jemismaru <jemismaru@gmail.com>
2022-01-05 14:42:21 +0530

    Webpage completed.

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function redirectFun() {
+    window.location.href='../html/index.html';
+}
+
+function changeEffect() {
+    document.getElementById('iconRocket').style.color = "white";
+    document.getElementById('col2').style.backgroundColor = "#e9204f";
+    document.getElementById('colHrG1').style.backgroundColor = "white";
+}
+
+function resetEffect() {
+    document.getElementById('iconRocket').style.color = "#e9204f";
+    document.getElementById('col2').style.backgroundColor = "#101010";
+    document.getElementById('colHrG1').style.backgroundColor = "#e9204f";
+}
+
+function changeEffect1() {
+    document.getElementById('iconPie').style.color = "white";
+    document.getElementById('shadDiv').style.backgroundColor = "#e9204f";
+    document.getElementById('colHrG2').style.backgroundColor = "white";
+}
+
+function resetEffect1() {
+    document.getElementById('iconPie').style.color = "#e9204f";
+    document.getElementById('shadDiv').style.backgroundColor = "#101010";
+    document.getElementById('colHrG2').style.backgroundColor = "#e9204f";
+}
